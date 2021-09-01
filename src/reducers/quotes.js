2aeeefb17a8006263738af220badd12c020eeda8
love.cosma <williamsmatthew333@gmail.com>
2021-09-01 10:08:25 -0600

    commit tree 1f1a73ed4b0defd4fe6b16aea6fa75669f5ce84f

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,38 @@
 export default (state = [], action) => {
-  return state;
+
+  switch(action.type){
+
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+
+    case "REMOVE_QUOTE":
+      let filteredArray = state.filter(quote => quote.id !== action.quoteId)
+      return [...filteredArray]
+
+    case "UPVOTE_QUOTE":
+      let afterQuoteLiked = state.map(quote => {
+        if(quote.id === action.quoteId){
+          quote.votes += 1
+        }
+        return quote
+      })  
+      return [...afterQuoteLiked]
+
+      case "DOWNVOTE_QUOTE":
+        let afterQuoteDown = state.map(quote => {
+          if(quote.id === action.quoteId){
+            quote.votes -= 1
+          }
+          if(quote.votes < 0){
+            quote.votes = 0
+          }
+          return quote
+        }) 
+        // debugger
+        return [...afterQuoteDown]
+
+    default: 
+    return state;
+  }
+
 }
